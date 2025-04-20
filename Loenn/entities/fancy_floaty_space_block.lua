cef78919e551e0df0e4a16ef5eed4b2b2252adcc
coffe789 <jonathan_davey@outlook.com>
2025-04-20 20:51:07 +1000

    fix floaty block loenn plugin

--- Loenn/entities/fancy_floaty_space_block.lua
+++ Loenn/entities/fancy_floaty_space_block.lua
@@ -1,11 +1,10 @@
-local fakeTilesHelper = require("helpers.fake_tiles")
 local mods = require("mods")
 local fancyTileEntitieshelper = mods.requireFromPlugin("libraries.fancy_tile_entities_helper")
 
 local floatySpaceBlock = {}
 
 floatySpaceBlock.associatedMods = {"FancyTileEntities"}
-floatySpaceBlock.name = "FancyTileEntities/floatySpaceBlock"
+floatySpaceBlock.name = "FancyTileEntities/FancyFloatySpaceBlock"
 floatySpaceBlock.depth = -9000
 floatySpaceBlock.placements = {
     name = "floaty_space_block",
@@ -16,18 +15,15 @@ floatySpaceBlock.placements = {
         randomSeed = 0,
         width = 8,
         height = 8
-    
+    }
 }
 
 floatySpaceBlock.fieldInformation = {
     tileData = {
-        fieldType = "FancyTileEntities.buttonStringField",
-        fte_passData = true
+        fieldType = "FancyTileEntities.buttonStringField"
     }
 }
 
 floatySpaceBlock.sprite = fancyTileEntitieshelper.getEntitySpriteFunction("blendEdges", "tilesFg", {1, 1, 1, 1})
-
-floatySpaceBlock.fieldInformation = fakeTilesHelper.getFieldInformation("connectsTo")
 
 return floatySpaceBlock
